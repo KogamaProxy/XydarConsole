b7f0ed5760bc5015018617c8cbcf0d639959ef06
KogamaProxy <104033723+KogamaProxy@users.noreply.github.com>
2022-04-20 23:48:59 +0200

    Create main-proxy.user.js

---
+++ main-proxy.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          Xydar Trainer
+// @version       0.1
+// @author        Xydar
+// @match         https://kogama.com.br/page/webgl-frame/*
+// @match         https://www.kogama.com/page/webgl-frame/*
+// @match         https://friends.kogama.com/page/webgl-frame/*
+// @require       https://raw.githubusercontent.com/KogamaProxy/XydarConsole/main/source.js
+// @require       https://pastebin.com/raw/NWL34qG0
+// ==/UserScript==
